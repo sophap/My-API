025ede365b74db03578426b4d2dd9c7db9709722
sammyremi <adebayorsamuelas@gmail.com>
2023-05-19 03:21:10 +0100

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "csv"
+
+CSV.foreach(Rails.root.join('volcano.csv'), headers: true) do |row|
+    Volcano.create!(
+        volcano_id: row['VolcanoID'],
+        volcano_name: row['V_Name'],
+        country: row['Country'],
+        region: row['Region'],
+        subregion: row['Subregion'],
+        latitude: row['Latitude'],
+        longitude: row['Longitude'],
+        pei: row['PEI'],
+        h_active: row['H_active'],
+        vei_holoce: row['VEI_Holoce'],
+        hazard: row['hazard'],
+        classification: row['class'],
+        risk: row['risk']
+    )
+end
